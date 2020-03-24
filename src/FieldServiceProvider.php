3cf284ce3fc6875606cddc6f1e183f319349b99a
Sasaya <storyn26383@gmail.com>
2020-03-24 23:26:26 +0800

    fontech

--- src/FieldServiceProvider.php
+++ src/FieldServiceProvider.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace Fourstacks\NovaRepeatableFields;
+namespace Fontech\NovaRepeatableFields;
 
-use Laravel\Nova\Nova;
-use Laravel\Nova\Events\ServingNova;
 use Illuminate\Support\ServiceProvider;
+use Laravel\Nova\Events\ServingNova;
+use Laravel\Nova\Nova;
 
 class FieldServiceProvider extends ServiceProvider
 {
